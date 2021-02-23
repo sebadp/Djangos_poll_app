b757d84e6f2a1dc9f9a0b4aa23351e8c67880d6c
seba <sebastian.davila.personal@gmail.com>
2021-02-23 06:50:18 -0300

    Admin lookup

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,9 @@ from .models import Question, Choice
 
 # Register your models here.
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    
 admin.site.register(Question)
 
 admin.site.register(Choice)
