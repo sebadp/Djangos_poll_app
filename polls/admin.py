b5c66779e86a9991ce50ea4a9cae90fce3f54650
seba <sebastian.davila.personal@gmail.com>
2021-02-23 12:35:32 -0300

    Admin lookup addons

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Question, Choice
+
+from .models import Choice, Question
 
 # Register your models here.
 
+
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
-    
+    fields = ["pub_date", "question_text"]
+
+
 admin.site.register(Question)
 
 admin.site.register(Choice)
